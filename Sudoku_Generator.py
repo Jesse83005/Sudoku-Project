4bff0307552ef4766ed86bc68a0663d932f3f35c
Elan <166648947+elanale@users.noreply.github.com>
2024-04-22 16:50:31 -0400

    generator1

---
+++ Sudoku_Generator.py
@@ -0,0 +1,44 @@
+
+class Generator:
+    def __init__(self, row_length, removed_cells):
+        self.row_length = row_length
+        self.removed_cells = removed_cells
+
+
+    def get_board(self):
+
+
+    def print_board(self):
+
+
+    def valid_in_row(self):
+
+
+    def valid_in_col(self):
+
+
+    def valid_in_box(self):
+
+
+    def is_valid(self):
+
+
+    def fill_box(self):
+
+
+    def fill_diagonal(self):
+
+
+    def fill_remaining(self):
+
+
+    def fill_values(self):
+
+
+    def remove_cells(self):
+
+
+    def generate_sudoku(self):
+
+
+
