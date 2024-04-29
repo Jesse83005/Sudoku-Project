892c4ec59ef971e2008a97256cccff3ccea7bda5
Elan <166648947+elanale@users.noreply.github.com>
2024-04-29 19:28:11 -0400

    Update sudoku.py
    created the main sudoku file to start the game

--- sudoku.py
+++ sudoku.py
@@ -1,2 +1,25 @@
-from board import Board
-import pygame
+from board import Board # Class
+from gui import SudokuWindow
+
+def main():
+    window = SudokuWindow()
+    # set the scene to grid and then display the menu
+    scene = "grid"
+    difficulty = window.menu()
+
+    while True:
+        if scene == "menu":
+            difficulty = window.menu()
+            scene = "grid"
+        elif scene == "grid":
+            scene = window.grid(Board(9, difficulty))
+        elif scene == "win":
+            window.win()
+        elif scene == "lose":
+            scene = window.loss()
+        else:
+            raise Exception("invalid")
+
+if __name__ == "__main__":
+    main()
+
